7e350185066e8eaf6638b7720d06990383f4890b
SoroushF79 <31623958+SoroushF79@users.noreply.github.com>
2018-01-05 14:00:10 -0600

    Update PrimeNumbers.py

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,10 +1,18 @@
-def check(a):
+# Input function. Makes sure that the inputs were numbers and weren't letters/symbols.
+def check():
+    global a
+    global b
     while True:
-        if a <= 1:
-            print("a is not greater than 1. Try again.")
-            prog()
-        else:
+        try:
+            a = int(input("a: "))
+            b = int(input("b: "))
             break
+        except ValueError:
+            print("Make sure a and b are positive integer numbers, not characters or non-integer numbers.")
+            prog()
+
+# ND(Normal Distribution) function. Asks if the user wants to display a normal distribution 
+# of the prime numbers from their interval.          
 def ND():
     global Questi
     Questi = input("Would you like to see the normal distribution of the prime numbers in your interval? ")
@@ -17,16 +25,23 @@ def ND():
     else:
         print("I did not understand that.")
         ND(Questi)
+
+# Histogram function. Does the same thing as the ND function except with an automaticallly
+# binned histogram.
 def Histo():
     global Questio
     Questio = input("Would you like to see the histogram of the prime numbers in your interval? ")
     if (Questio.lower() == "yes") or (Questio.lower() == "y"):
         plt.hist(my_list, bins="auto")
+        plt.show()
     elif (Questio.lower() == "no") or (Questio.lower() == "n"):
         print("OK")
     else:
         print("I did not understand that.")
         Histo(Questio)
+
+# Asks if the user actually wants to see the prime numbers from their interval. Displayed 
+# in a list (my_list).
 def finish():
     global Quest
     Quest = input("Would you like to see all the prime numbers from your interval? ")
@@ -38,6 +53,8 @@ def finish():
     else:
         print("I did not understand that.")
         finish(Quest)
+
+# Makes sure that a was greater than or equal to b.
 def checkrel(a, b):
     while True:
         if a >= b:
@@ -45,20 +62,27 @@ def checkrel(a, b):
             prog()
         else:
             break
+        
+# Makes sure that a is greater than 1.
+def gre():
+    while True:
+        if a > 1:
+            break
+        else:
+            print("Make sure a is greater than 1.")
+        
+# "Actual" program
 def prog():
-    Prompt = "Choose an interval a and b, such that 1 < a < b: "
-    global a
-    global b
+    Prompt = "Choose an interval a and b, such that 1 < a < b and a and b are integers: "
     global p
     global mean
     global sd
     print(Prompt)
-    a = input("a: ")
-    b = input("b: ")
-    check(int(a))
-    checkrel(int(a), int(b))
+    check()
+    gre()
+    checkrel(a, b)
     p = 0
-    for x in range(int(a), int(b) +1):
+    for x in range(int(a), int(b) + 1):
         for y in range(2, int(x ** 0.5) + 1):
             if x % y == 0:
                 break
@@ -73,11 +97,14 @@ def prog():
     print(("The mean and median of the prime numbers in your interval are %f and %d, respectively.") % (mean, median))
     print(("The standard deviaation of the prime numbers in your interval is %f.") % (sd))
     finish()
+    
 import sys
 import statistics
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.mlab as mlab
+a = 0
+b = 0
 my_list = [] 
 prog()
 ND()
